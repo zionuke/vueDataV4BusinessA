bf68525de6a02212b4286d6a32b70cf43d7c1be5
zionuke <VergilPower8@outlook.com>
2023-03-11 21:31:35 +0800

    调整商家销售统计柱状图UI

--- visualization/src/components/Seller.vue
+++ visualization/src/components/Seller.vue
@@ -26,7 +26,7 @@ export default {
   methods: {
     // 初始化echartInstance对象
     initChart () {
-      this.chartInstance = this.$echarts.init(this.$refs.seller_ref)
+      this.chartInstance = this.$echarts.init(this.$refs.seller_ref, 'chalk')
       // 对图表对象进行鼠标事件的监听
       this.chartInstance.on('mouseover', () => {
         clearInterval(this.timerId)
@@ -56,6 +56,21 @@ export default {
       const sellerNames = showData.map(item => item.name)
       const sellerValues = showData.map(item => item.value)
       const option = {
+        title: {
+          text: '▎商家销售统计',
+          textStyle: {
+            fontSize: 66
+          },
+          left: 20,
+          top: 20
+        },
+        grid: {
+          top: '20%',
+          left: '3%',
+          right: '6%',
+          bottom: '3%',
+          containLabel: true // 距离包含坐标轴上的文字
+        },
         xAxis: {
           type: 'value'
         },
@@ -63,10 +78,49 @@ export default {
           type: 'category',
           data: sellerNames
         },
+        // 柱状图条目背景
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            z: 0,
+            lineStyle: {
+              width: 66,
+              color: '#2D3443'
+            }
+          }
+        },
         series: [
           {
             type: 'bar',
-            data: sellerValues
+            data: sellerValues,
+            barWidth: 66, // 柱状图条目宽度
+            // 柱状图条目文字
+            label: {
+              show: true,
+              position: 'right',
+              textStyle: {
+                color: 'white'
+              }
+            },
+            itemStyle: {
+              // 柱状图条目圆角，左上，右上，右下，左下
+              barBorderRadius: [0, 33, 33, 0],
+              // 指明颜色渐变的方向
+              // 指明不同百分比之下颜色的值
+              color: new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                // 百分之0状态之下的颜色值
+                {
+                  offset: 0,
+                  color: '#5052EE'
+                },
+                // 百分之100状态之下的颜色值
+                {
+                  offset: 1,
+                  color: '#AB6EE5'
+                }
+              ])
+            }
           }
         ]
       }
