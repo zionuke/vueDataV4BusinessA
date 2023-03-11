b1e8e44bf9751936506ff8eba87d6990ca86ec86
zionuke <VergilPower8@outlook.com>
2023-03-11 20:59:26 +0800

    商家销售统计柱状图实现动态刷新

--- visualization/src/components/Seller.vue
+++ visualization/src/components/Seller.vue
@@ -10,29 +10,51 @@ export default {
   data () {
     return {
       chartInstance: null,
-      allData: null
+      allData: null, // 服务器返回的数据
+      currentPage: 1, // 当前显示的页数
+      totalPage: 0, // 一共有多少页
+      timerId: null // 定时器的标识
     }
   },
   mounted () {
     this.initChart()
     this.getData()
   },
+  beforeDestroy () {
+    clearInterval(this.timerId)
+  },
   methods: {
     // 初始化echartInstance对象
     initChart () {
       this.chartInstance = this.$echarts.init(this.$refs.seller_ref)
+      // 对图表对象进行鼠标事件的监听
+      this.chartInstance.on('mouseover', () => {
+        clearInterval(this.timerId)
+      })
+      this.chartInstance.on('mouseout', () => {
+        this.startInterval()
+      })
     },
     // 获取服务器的数据
     async getData () {
       // http://127.0.0.1:8888/api/seller
       const { data: ret } = await this.$http.get('seller')
       this.allData = ret
+      // 对数据排序
+      this.allData.sort((a, b) => a.value - b.value) // 从小到大的排序
+      // 每5个元素显示一页
+      this.totalPage = this.allData.length / 5 === 0 ? this.allData.length / 5 : Math.ceil(this.allData.length / 5)
       this.updateChart()
+      // 启动定时器
+      this.startInterval()
     },
     // 更新图表
     updateChart () {
-      const sellerNames = this.allData.map(item => item.name)
-      const sellerValues = this.allData.map(item => item.value)
+      const start = (this.currentPage - 1) * 5
+      const end = this.currentPage * 5
+      const showData = this.allData.slice(start, end)
+      const sellerNames = showData.map(item => item.name)
+      const sellerValues = showData.map(item => item.value)
       const option = {
         xAxis: {
           type: 'value'
@@ -49,6 +71,18 @@ export default {
         ]
       }
       this.chartInstance.setOption(option)
+    },
+    startInterval () {
+      if (this.timerId) {
+        clearInterval(this.timerId)
+      }
+      this.timerId = setInterval(() => {
+        this.currentPage++
+        if (this.currentPage > this.totalPage) {
+          this.currentPage = 1
+        }
+        this.updateChart()
+      }, 3000)
     }
   }
 }
